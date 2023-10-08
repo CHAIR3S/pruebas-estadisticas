d7c67eeedb6eba3cf548fb041a23175cb93575bf
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-10-07 21:47:55 -0600

    Library Math-3 from Apache commons ready to use

---
+++ src/main/java/org/example/utils/Operations.java
@@ -0,0 +1,23 @@
+package org.example.utils;
+
+public class Operations {
+
+    public double sumarDatosMatriz( double[][] matriz ){
+        double suma = 0;
+        int indexV, indexH;
+
+        for ( indexV = 0; indexV < matriz.length; indexV++) {
+            for ( indexH = 0; indexH < matriz.length; indexH++) {
+                suma += matriz[indexV][indexH];
+            }
+        }
+
+        return suma;
+    }
+
+    public double promediarDatosMatriz( double[][] matriz ){
+        double suma = sumarDatosMatriz(matriz);
+
+        return ( suma / ( matriz.length * matriz.length ));
+    }
+}
