508cab43cf22611ee2c921365c9e2366d75f8702
Luis Leal <luisrrleal@gmail.com>
2023-10-16 14:54:51 -0600

    Merge with development

--- src/main/java/org/example/MatrizCuadrada.java
+++ src/main/java/org/example/MatrizCuadrada.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 
 public class MatrizCuadrada {
     int numeroBase, sumaDatos, promedioDatos;
-    public double[][] matriz = 
-    	{{.320,.726,.342,.016},
-    	{.018,.567,.515,.476},
-    	{.671,.302,.876,.854},
-    	{.083,.516,.524,.976}};
+    public double[][] matriz = { { .320, .726, .342, .016 },
+            { .018, .567, .515, .476 },
+            { .671, .302, .876, .854 },
+            { .083, .516, .524, .976 } };
 
     MatrizCuadrada(int p_numeroBase) {
         this.numeroBase = p_numeroBase;
